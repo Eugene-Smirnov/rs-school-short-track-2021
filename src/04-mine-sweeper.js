b9b7270835bc205a7cc0bebdb69951293c6cba4b
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-04-07 23:42:35 +0300

    feat: implement task04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,35 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i].push(0);
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const notMarginalVector = i - 1 !== -1;
+      const notLeftMarginalVal = j - 1 !== -1;
+      const notRightMarginalVal = j + 1 <= matrix[i].length;
+      if (matrix[i][j]) {
+        if (notMarginalVector) {
+          if (notLeftMarginalVal) result[i - 1][j - 1] += 1;
+          if (notRightMarginalVal) result[i - 1][j + 1] += 1;
+          result[i - 1][j] += 1;
+        }
+        if (i + 1 <= matrix.length) {
+          if (notLeftMarginalVal) result[i + 1][j - 1] += 1;
+          if (notRightMarginalVal) result[i + 1][j + 1] += 1;
+          result[i + 1][j] += 1;
+        }
+        if (notLeftMarginalVal) result[i][j - 1] += 1;
+        if (notRightMarginalVal) result[i][j + 1] += 1;
+      }
+    }
+  }
+  return result;
 }
-
 module.exports = minesweeper;
