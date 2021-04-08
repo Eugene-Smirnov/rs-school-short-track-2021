7698a13195fc4816b6354e8bf0bc1370d7797702
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-04-08 14:10:38 +0300

    feat: implement task08

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,14 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  const list = l;
+  if (list.value === k) {
+    list.value = list.next.value;
+    list.next = list.next.next;
+  }
+  if (list.next !== null) removeKFromList(list.next, k);
+  return list;
 }
 
 module.exports = removeKFromList;
