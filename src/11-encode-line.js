38a145d364e3299d8f8c8c5c7815c0c406ea7d1a
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-04-09 12:48:52 +0300

    feat: implement task11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    let counter = '';
+    if (str[i] === str[i + 1]) {
+      counter = 1;
+      for (let j = i + 1; j < str.length; j++) {
+        if (str[i] === str[j]) {
+          counter++;
+        } else {
+          break;
+        }
+      }
+      result.push(`${counter}${str[i]}`);
+      counter--;
+    } else {
+      result.push(str[i]);
+    }
+    i += +counter;
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
