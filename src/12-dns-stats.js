8a4997653cb774be5db302d5a535552b39f4e249
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-04-11 00:45:00 +0300

    feat: implement task12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,43 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function uniqueDNS(domain) {
+  const result = [];
+  const index = domain.lastIndexOf('.');
+  let head;
+  if (index !== -1) {
+    result.push(domain.slice(index));
+    head = domain.slice(0, index);
+    if (head.lastIndexOf('.') !== -1) {
+      result.push(...uniqueDNS(head));
+    } else {
+      result.push(`.${head}`);
+    }
+  }
+  return result;
+}
+function uniteDNS(array) {
+  const arr = array;
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i - 1] + arr[i];
+  }
+  return arr;
+}
+function getDNSStats(domains) {
+  const result = {};
+  const array = [];
+  for (let i = 0; i < domains.length; i++) {
+    array.push(...uniteDNS(uniqueDNS(domains[i])));
+  }
+  let set = new Set(array);
+  set = Array.from(set.values());
+  for (let i = 0; i < set.length; i++) {
+    result[`${set[i]}`] = 0;
+    for (let j = 0; j < array.length; j++) {
+      if (set[i] === array[j]) result[`${set[i]}`]++;
+    }
+  }
+  return result;
 }
 
 module.exports = getDNSStats;
